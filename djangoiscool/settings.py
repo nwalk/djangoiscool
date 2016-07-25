be5cde6d23b4af275bec87aecbb126abdca92a5e
nwalk <nwalk19@gmail.com>
2016-07-25 00:47:14 -0400

    up and running
    the home page is working...

--- djangoiscool/settings.py
+++ djangoiscool/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.website',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'djangoiscool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'djangoiscool/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
