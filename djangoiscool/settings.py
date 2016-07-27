49654c84158f0a52fcc7dff1b86aecb9ef7b483f
Nathan Walker <nwalk19@gmail.com>
2016-07-26 22:37:27 -0400

    404, 500, and bootstrap nav-bar
    django will use the 400.html, and the 500.html based
    on the location of the files, no other code required.
    for a more complicated view you will need to set;
    handler404 = 'views.custom_404_view' in your URLconf.
    the same will apply to the 500, 403, and 400 code
    I added the bootstrap navbar (fixed), and all the css
    classes in the style_sheet used by the navbar (as far as i know).

--- djangoiscool/settings.py
+++ djangoiscool/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '{}'.format(generate_key(40, 128))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.0']
 
 
 # Application definition
